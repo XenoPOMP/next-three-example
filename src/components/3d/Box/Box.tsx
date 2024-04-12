26c99af43dc499e613910f813984eb0df4eddab6
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-12 14:01:45 +0300

    🖼️ add textures

--- src/components/3d/Box/Box.tsx
+++ src/components/3d/Box/Box.tsx
@@ -1,9 +1,14 @@
 import { useFrame, useLoader } from '@react-three/fiber';
 import type { VariableFC } from '@xenopomp/advanced-types';
 import { useMemo, useState } from 'react';
-import { Euler, TextureLoader } from 'three';
+import { Euler, MeshPhysicalMaterial, TextureLoader } from 'three';
 
-import boxTexture from '@/public/textures/img.png';
+import gleb1Img from '@/public/textures/1.png';
+import gleb2Img from '@/public/textures/2.png';
+import gleb3Img from '@/public/textures/3.png';
+import gleb4Img from '@/public/textures/4.png';
+import gleb5Img from '@/public/textures/5.png';
+import lupaglazikImg from '@/public/textures/lupaglazik.png';
 
 import type { BoxProps } from './Box.props';
 
@@ -16,7 +21,7 @@ const Box: VariableFC<'mesh', BoxProps, 'rotation'> = ({
   const [rotateX, setRotateX] = useState(0);
   const [rotateY, setRotateY] = useState(0);
   const [rotateZ, setRotateZ] = useState(0);
-  const rotationStep = useMemo(() => 0.01, []);
+  const rotationStep = useMemo(() => 0.011, []);
 
   // Animation
   useFrame(() => {
@@ -28,18 +33,29 @@ const Box: VariableFC<'mesh', BoxProps, 'rotation'> = ({
   // Size of box
   const size = 3;
 
-  const texture = useLoader(TextureLoader, boxTexture.src);
+  const lupaglazikTexture = useLoader(TextureLoader, lupaglazikImg.src);
+  const gleb1Texture = useLoader(TextureLoader, gleb1Img.src);
+  const gleb2Texture = useLoader(TextureLoader, gleb2Img.src);
+  const gleb3Texture = useLoader(TextureLoader, gleb3Img.src);
+  const gleb4Texture = useLoader(TextureLoader, gleb4Img.src);
+  const gleb5Texture = useLoader(TextureLoader, gleb5Img.src);
 
   return (
     <mesh
       receiveShadow={receiveShadow}
       castShadow={castShadow}
       rotation={new Euler(rotateX, rotateY, rotateZ)}
+      material={[
+        new MeshPhysicalMaterial({ map: lupaglazikTexture }),
+        new MeshPhysicalMaterial({ map: gleb1Texture }),
+        new MeshPhysicalMaterial({ map: gleb2Texture }),
+        new MeshPhysicalMaterial({ map: gleb3Texture }),
+        new MeshPhysicalMaterial({ map: gleb4Texture }),
+        new MeshPhysicalMaterial({ map: gleb5Texture }),
+      ]}
       {...props}
     >
       <boxGeometry args={[size, size, size]} />
-
-      <meshPhysicalMaterial map={texture} />
     </mesh>
   );
 };

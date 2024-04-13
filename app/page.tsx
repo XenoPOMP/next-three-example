a3ab79c38e4e4d70c3db31879e7f22efc217e517
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-13 13:17:46 +0300

    🔃 add loading label

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import { type Metadata } from 'next';
+import { Suspense } from 'react';
 
 import Scene from '@/src/components/3d/Scene';
 import { generateOpenGraph, generateStaticMetadata } from '@/src/utils/seo';
@@ -15,7 +16,18 @@ export const metadata: Metadata = generateStaticMetadata({
 export default function Home() {
   return (
     <main className={cn(styles.mainPage)}>
-      <Scene />
+      <Suspense
+        fallback={
+          <div
+            aria-hidden
+            className={cn('fixed w-screen h-dvh flex-center')}
+          >
+            Loading...
+          </div>
+        }
+      >
+        <Scene />
+      </Suspense>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const tailwindConfig: Config = {
     CustomClassesPlugin(),
     tailwindThemer({
       defaultTheme: {
-        extend: lightTheme,
+        extend: darkTheme,
       },
       themes: [
         {
